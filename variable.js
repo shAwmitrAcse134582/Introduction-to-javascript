9632f599942e69b29b1c66750c4d4e337e123eb1
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-01-17 21:23:24 +0600

    initial

---
+++ variable.js
@@ -0,0 +1,17 @@
+var age = 21;
+console.log(age);
+var name = 'shawmitra';
+console.log(name);
+var location = "Barishal";
+console.log(location);
+
+var issingle = true;
+console.log(issingle);
+
+//1.variable name can't be any keyword
+//2. variable name has to be oin one reserveword
+//3.variabe nname can not have qu0tation
+//4. variabe can not be stat with a number
+//5.can not use dash but u can use underscore
+// javascript user  use cammel case
+// javascript is high level language and variabe name is case sensative
