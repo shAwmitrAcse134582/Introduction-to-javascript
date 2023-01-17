9d6b9f6a7f7e2dc01af9a8ea3bab1550c199e956
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-01-18 00:44:11 +0600

    math

---
+++ practise.js
@@ -0,0 +1,24 @@
+// problem-1
+var money = 1000;
+var fruits = 700;
+var returnMoney = money - fruits;
+console.log(returnMoney);
+
+//problem-2
+
+var Mathematics = 75.25;
+var Biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var Bangla = 99.50;
+var totalMarks = Mathematics + Biology + chemistry + physics + Bangla;
+var avarageMarks = totalMarks / 5;
+avarageMarks = avarageMarks.toFixed(2);
+console.log(avarageMarks);
+
+//problem-3
+var s1 = "I am going to be";
+var s2 = "an awesome web developer";
+var s3 = s1 + ' ' + s2;
+console.log(s3);
+
