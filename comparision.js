151e695e40d50c0d9b1ddece20b2800353a168b8
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-01-18 16:28:02 +0600

    condition

---
+++ comparision.js
@@ -0,0 +1,14 @@
+console.log(5 < 8);
+console.log(5 == 8);
+console.log(5 != 8);
+
+//Multiple condition
+var a = 10;
+var b = 15;
+var c = 12;
+if (a > b && a > c)
+    console.log("a is biggest number");
+else if (b > a && b > c)
+    console.log("b is the  biggest number");
+else
+    console.log("c is the biggest number");
