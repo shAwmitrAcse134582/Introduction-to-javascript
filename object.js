0d5a024abc7889c0bc79330f839ce92a82f95c65
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-01-19 20:01:52 +0600

    object

---
+++ object.js
@@ -0,0 +1,35 @@
+//crate object by javascript
+var student = {
+    name: 'shawmitra',
+    roll: 33,
+    dept: 'cse',
+    Address: 'Barishal'
+}
+console.log(student);
+
+//find out specific property
+
+var shopping = {
+    pant: 1600,
+    shirt: 1500,
+    jacket: 2500,
+    shoe: 3200
+}
+console.log(shopping.shirt);
+
+shopping.shoe = 4000;
+console.log(shopping.shoe);
+
+//how to get only keys or values
+var mobile = {
+    brand: 'xiomi',
+    price: 24000,
+    ram: '6gb',
+    camera: '48px'
+
+}
+var mobileKeys = Object.keys(mobile);
+console.log(mobileKeys);
+
+var mobileValues = Object.values(mobile);
+console.log(mobileValues);
