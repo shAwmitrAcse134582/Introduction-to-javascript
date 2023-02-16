b285ec4da215ac51825499cd608c5b5fe049812a
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-02-16 09:20:53 +0600

    chainning

---
+++ chain.js
@@ -0,0 +1,21 @@
+const users = [{
+    id: 1,
+    name: 'shawmitra',
+    job: 'Software Engineer',
+}]
+
+console.log(users[0].job);
+
+const data = {
+    count: 500,
+    data: [
+        {
+            id: 1, name: 'Bijon', job: 'freelancer'
+        },
+        {
+            id: 2, name: 'tanbir', job: 'businessman'
+        },
+    ]
+}
+
+console.log(data.data[1].name);
